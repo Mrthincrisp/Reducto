15f88549a6481f2da589b7c30d15b00f5b0f5ede
Derek Swann <148038820+Mrthincrisp@users.noreply.github.com>
2024-07-09 22:01:58 -0500

    Add project files.

---
+++ Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reducto
+{
+    public class Products
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public bool Sold { get; set; }
+        public ProductType? ProductType { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace Reducto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var wand = new ProductType
+            {
+                Id = "wan",
+                Name = "Wand"
+            };
+            var apparel = new ProductType
+            {
+                Id = "app",
+                Name = "Apparel"
+            };
+            var potion = new ProductType
+            {
+                Id = "pot",
+                Name = "Potion"
+            };
+            var enchant = new ProductType
+            {
+                Id = "enchant",
+                Name = "Enchanted Object"
+            };
+
+            List<Products> products =
+            [
+                new Products()
+                {
+                    Name = "Pheonix Core Wand",
+                    Price = 12.25m,
+                    Sold = false,
+                    ProductType = wand
+                },
+                new Products()
+                {
+                    Name = "Blue Cloak",
+                    Price = 25.05m,
+                    Sold = false,
+                    ProductType = apparel
+                },
+                new Products()
+                {
+                    Name = "Love Potion",
+                    Price = 30.27m,
+                    Sold = false,
+                    ProductType = potion
+                },
+                new Products()
+                {
+                    Name = "Boots with the fur",
+                    Price = 49.99m,
+                    Sold = false,
+                    ProductType = apparel
+                },
+                new Products()
+                {
+                    Name = "Flying Broom",
+                    Price = 14.99m,
+                    Sold = true,
+                    ProductType = enchant
+                }
+            ];
+
+            string greeting = "Hey, here's the Main Menu:";
+
+            Console.WriteLine(greeting);
+            string choice = null;
+            while (choice != "0")
+            {
+                Console.WriteLine(@"1. View all products
+2. Add a new Product
+3. Delete a Product
+4. Update a Product
+0. Quit");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("Bye.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "1":
+                        ShowAllProducts();
+                        break;
+                    case "2":
+                        //
+                        break;
+                    case "3":
+                        //
+                        break;
+                    case "4":
+                        //
+                        break;
+                    default:
+                        Console.WriteLine("PLEASE MAKE A SELECTION BETWEEN 0-4");
+                        Console.Write("Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+            void ShowAllProducts()
+            {
+                for (int i = 0; i < products.Count; i++){
+                    Console.WriteLine($"{i + 1}. {products[i].Name} : ${products[i].Price}.");
+                }
+                    Console.Write("Press any key to continue.");
+                    Console.ReadKey();
+            }
+
+
+        }
+    }
+}
